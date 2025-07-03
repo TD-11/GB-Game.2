3a260e8cd86ffd5938e54cece6890199b5985bc6
TD-11 <tulio.s@escolar.ifrn.edu.br>
2025-07-03 15:15:16 -0300

    Queda de objetos incompleto

--- Jogo.2/Assets/Scripts/FallingObjects1.cs
+++ Jogo.2/Assets/Scripts/FallingObjects1.cs
@@ -5,13 +5,20 @@ public class FallingObjects1 : MonoBehaviour
     public GameObject fallingObjectPrefab; // Prefab do objeto que vai cair
     public GameObject alert;
     public Transform player; // Referência ao jogador
+    public Rigidbody2D rg;
+    public float fallingSpeed = 3f;
     public float spawnInterval = 7f; // Tempo entre cada instância
     public float spawnRangeX = 5f; // Largura horizontal do spawn
     public float spawnHeight = 10f; // Altura onde os objetos aparecerão
     public float spawnAlert = 1f; // Altura onde os objetos aparecerão
 
+    
     private float timer;
 
+    void Start()
+    {
+        
+    }
     void Update()
     {
         timer += Time.deltaTime;
@@ -23,11 +30,16 @@ public class FallingObjects1 : MonoBehaviour
         }
     }
 
+    void FixedUpdate()
+    {
+        
+    }
+
     void SpawnObject()
     {
         float x = Random.Range(-spawnRangeX, spawnRangeX);
-        Vector3 spawnPosition1 = new Vector3(player.position.x + x, player.position.y + spawnHeight, 0f);
-        Vector3 spawnPosition2 = new Vector3(player.position.x + x, player.position.y + spawnAlert, 0f);
+        Vector2 spawnPosition1 = new Vector2(player.position.x + x, player.position.y + spawnHeight);
+        Vector2 spawnPosition2 = new Vector2(player.position.x + x, player.position.y + spawnAlert);
         Instantiate(fallingObjectPrefab, spawnPosition1, Quaternion.identity);
         Instantiate(alert, spawnPosition2, Quaternion.identity);
     }

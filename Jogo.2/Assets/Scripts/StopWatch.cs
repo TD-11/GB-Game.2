b69c60e43dd6c4460dc8a2be5b8c2a935bd62a62
TD-11 <tulio.s@escolar.ifrn.edu.br>
2025-07-10 14:06:38 -0300

    Fonte do cronômetro adicionada

--- Jogo.2/Assets/Scripts/StopWatch.cs
+++ Jogo.2/Assets/Scripts/StopWatch.cs
@@ -5,21 +5,21 @@ using UnityEngine.UI;
 public class StopWatch : MonoBehaviour
 {
     public GameObject player;
-    public TMP_Text textoTempo; // Arraste um Text da UI aqui no Inspector
+    public TMP_Text textoTempo; 
     private float tempo = 0f;
     
 
     void Update()
     {
-        if (player == null) // quando o personagem for destruído
+        if (player == null) // Quando o personagem for destruído
         {
-            Time.timeScale = 0f; // pausa o tempo global
+            Time.timeScale = 0f; // Pausa o tempo global
         }
 
         tempo += Time.deltaTime;
-        int minutos = Mathf.FloorToInt(tempo / 60);
-        int segundos = Mathf.FloorToInt(tempo % 60);
+        int minutos = Mathf.FloorToInt(tempo / 60);// Converte o tempo em minutos
+        int segundos = Mathf.FloorToInt(tempo % 60);// Converte o tempo em segundos
 
-        textoTempo.text = string.Format("{0:00}:{1:00}", minutos, segundos);
+        textoTempo.text = string.Format("{0:00}:{1:00}", minutos, segundos);// espõe os valores no texto da interface
     }
 }

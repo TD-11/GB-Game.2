e4f28edde3c8e83ad200b851beee84a9727404b6
TD-11 <tulio.s@escolar.ifrn.edu.br>
2025-07-10 15:52:03 -0300

    Cronômetro está dando erro

--- Jogo.2/Assets/Scripts/StopWatch.cs
+++ Jogo.2/Assets/Scripts/StopWatch.cs
@@ -1,4 +1,5 @@
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,42 +7,49 @@ public class StopWatch : MonoBehaviour
 {
     public TMP_Text textTime;
     public GameObject player;
+    public GameObject defeatScreen;
     public float restTime = 60f; // tempo em segundos
     public Color normalColor = Color.white;
     public Color alertColor = Color.red;
     public float tempoDeAviso = 11f;
     private bool activeTime = true;
-
+    
     void Update()
     {
-        if (player == null)
-        {
-        activeTime = false;
-        }
-
         if (activeTime)
         {
-            restTime -= Time.deltaTime;
-            int minutos = Mathf.FloorToInt(restTime / 60);
-            int segundos = Mathf.FloorToInt(restTime % 60);
-
-            textTime.text = string.Format("{0:00}:{1:00}", minutos, segundos);
-            
-            if (restTime <= tempoDeAviso)
+            if (player == null )
             {
-                textTime.color = alertColor;
+                activeTime = false;
+            }
+
+            if (activeTime)
+            {
+                restTime -= Time.deltaTime;
+                int minutos = Mathf.FloorToInt(restTime / 60);
+                int segundos = Mathf.FloorToInt(restTime % 60);
+
+                textTime.text = string.Format("{0:00}:{1:00}", minutos, segundos);
+            
+                if (restTime <= tempoDeAviso)
+                {
+                    textTime.color = alertColor;
+                }
+                else
+                {
+                    textTime.color = normalColor;
+                }
             }
             else
             {
-                textTime.color = normalColor;
+                textTime.text = "00:00";
+                textTime.color = alertColor; // Cor final
+
+                activeTime = false;
+                defeatScreen.SetActive(true);
+                // Aqui você pode chamar Game Over ou qualquer outra lógica
             }
         }
-        else
-        {
-            textTime.text = "00:00";
-            textTime.color = alertColor; // Cor final
-            Time.timeScale = 0f;
-            // Aqui você pode chamar Game Over ou qualquer outra lógica
-        }
+        
     }
 }

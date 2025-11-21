9a2777f3c6a50be3818a286aff70ffd2535e2d54
TD-11 <tulio.s@escolar.ifrn.edu.br>
2025-11-21 12:11:50 -0300

    Audio de game over colocado

--- Jogo.2/Assets/Scripts/Game Controllers/StopWatch.cs
+++ Jogo.2/Assets/Scripts/Game Controllers/StopWatch.cs
@@ -41,22 +41,13 @@ public class StopWatch : MonoBehaviour, ITimeSubject
 
     void Update()
     {
-        if (player == null && !isDead)
-        {
-            playerAudioTime.PlayOneShot(GameOverSound, 3f); // Som de Game Over
-            isDead = true;
-        }
-        else
-        {
-            isDead = false;
-        }
-        
         // Se o tempo está pausado, não faz nada
         if (!activeTime) return;
 
         // Se o jogador for destruído/perdido, o cronômetro pára e avisa os observadores
         if (player == null)
         {
+            playerAudioTime.PlayOneShot(GameOverSound, 3f); // Som de Game Over
             activeTime = false;
             NotifyTimeEnded();
             return;

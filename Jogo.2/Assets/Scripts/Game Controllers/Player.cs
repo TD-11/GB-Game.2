e8bfe851904cd708e3310c342ba8822e040635e0
TD-11 <tulio.s@escolar.ifrn.edu.br>
2025-11-21 12:15:33 -0300

    Falta testar o som de desconexão

--- Jogo.2/Assets/Scripts/Game Controllers/Player.cs
+++ Jogo.2/Assets/Scripts/Game Controllers/Player.cs
@@ -39,8 +39,8 @@ public class Player : MonoBehaviour
     private AudioClip obstacleSound;
     [SerializeField]
     private AudioClip LifeSound;
-    //[SerializeField]
-    //private AudioClip GameOverSound;
+    [SerializeField]
+    private AudioClip GameOverSound;
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
@@ -68,6 +68,7 @@ public class Player : MonoBehaviour
                 // Se reconectou: volta ao modo automático
                 manualMode = false;
                 fallConnectionScreen.SetActive(false);
+                playerAudio.PlayOneShot(GameOverSound, 3f); // Som de Game Over
                 Time.timeScale = 1f;
 
                 Debug.Log("Balance Board conectada! Jogo normalizado!");
@@ -119,8 +120,6 @@ public class Player : MonoBehaviour
                 Destroy(gameObject);
                 Time.timeScale = 0f;
                 gameOverScreen.SetActive(true);
-                //playerAudio.PlayOneShot(GameOverSound, 3f); // Som de Game Over
-
             }
         }
 
